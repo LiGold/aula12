834f6ceba997f2a08efb688145ee974aea48a2cd
LiGold <43589104+LiGold@users.noreply.github.com>
2018-11-08 20:15:10 -0200

    inclui meu contrato MetaMask

--- usodeimagem.js
+++ usodeimagem.js
@@ -58,7 +58,7 @@ const contratoUsoDeImagemABI = [
 	}
 ];
 
-var contratoUsoDeImagem = web3.eth.contract(contratoUsoDeImagemABI).at("0x9d00df94db52803bf0fcdb248dd4db3bb25f0296");
+var contratoUsoDeImagem = web3.eth.contract(contratoUsoDeImagemABI).at("0x881Dd2E08b358368A5f0898FD251013616c1F76C");
 
 function obtemNomeEmpresa() {
     contratoUsoDeImagem.nomeEmpresa({from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
